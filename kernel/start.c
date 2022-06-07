e3ea57730124ee4457f18bd46e13d2a8dbc083d9
Lucas Geoffroy <lucas.geoffroy@grenoble-inp.org>
2022-06-07 17:58:45 +0200

    feat(kernel): Implement an example of switch context

--- kernel/start.c
+++ kernel/start.c
@@ -1,27 +1,48 @@
 #include "debugger.h"
 #include "cpu.h"
+#include "process.h"
+#include "scheduler.h"
+#include "ctx_sw.h"
 #include "screen.h"
 #include "../shared/printf.c"
 
-int fact(int n)
+void idle(void)
 {
-	if (n < 2)
-		return 1;
-
-	return n * fact(n-1);
+    printf("[idle] je tente de passer la main a proc1...\n");
+    ctx_sw(processTable[0].context, processTable[1].context);
 }
 
+void proc1(void)
+{
+    printf("[proc1] idle m'a donne la main\n");
+    printf("[proc1] j'arrete le systeme\n");
+    hlt();
+}
 
 void kernel_start(void)
 {
-	int i;
-	// call_debugger(); useless with qemu -s -S
+	Process idleP, proc1P;
 
-	i = 10;
+	// Initialise le processus idle
+	idleP.pid = 0;
+	idleP.state = RUNNING;
+	idleP.name = "idle";
 
-	efface_ecran();
-	printf("Hugo est tres beau");
-	printf("\n%d", fact(i));
+	processTable[0] = idleP;
+
+	// Place le processus idle comme processus élu
+	runningProcess = &processTable[0];
+
+	// Initialise le processus proc1
+	proc1P.pid = 1;
+	proc1P.state = READY;
+	proc1P.name = "proc1";
+	proc1P.executionStack[0] = (uint32_t) proc1;
+	proc1P.context[1] = (uint32_t) &(proc1P.executionStack[0]);
+	processTable[1] = proc1P;
+
+
+	idle();
 
 	while(1)
 	  hlt();
