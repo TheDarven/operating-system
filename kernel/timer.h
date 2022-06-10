d914b925157261d77d747685ed46e664590fd9ee
gaillalo <loic.gaillard1@grenoble-inp.org>
2022-06-10 09:16:17 +0200

    feat(kernel): add the scheduler in the timer.
    Authors: Loïc, Lucas Z, Lucas G

--- kernel/timer.h
+++ kernel/timer.h
@@ -1,8 +1,10 @@
+#ifndef __H_TIMER__
+#define __H_TIMER__
+
 #include "clock.h"
-#include "debug.h"
-#include "screen.h"
 #include "cpu.h"
 #include "scheduler.h"
-#include "ctx_sw.h"
 
 void timer_traitement(void);
+
+#endif
